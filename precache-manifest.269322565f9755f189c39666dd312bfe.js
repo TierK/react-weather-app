275e833d83d1deb907ba4a669099da8450f542f9
“kim” <kimbfsd@gmail.com>
2019-11-12 02:22:14 +0200

    Updates

---
+++ precache-manifest.269322565f9755f189c39666dd312bfe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27d081c27ff2c5b13395461467ba568",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "6869f08cc23960bb2806",
+    "url": "/react-weather-app/static/css/main.e7a78316.chunk.css"
+  },
+  {
+    "revision": "5c50521b05e90dabe252",
+    "url": "/react-weather-app/static/js/2.f6d751da.chunk.js"
+  },
+  {
+    "revision": "6869f08cc23960bb2806",
+    "url": "/react-weather-app/static/js/main.de690320.chunk.js"
+  },
+  {
+    "revision": "a6999c35007cc5a0fc1b",
+    "url": "/react-weather-app/static/js/runtime-main.eb7bbae3.js"
+  }
+]);
